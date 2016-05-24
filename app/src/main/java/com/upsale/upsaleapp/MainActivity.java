2634309a16231249c47b8d7cb6da531170990ec2
MatheusCS <cs.matheus07@gmail.com>
2016-05-24 12:15:58 -0300

    fluxo telas 2

--- app/src/main/java/com/upsale/upsaleapp/MainActivity.java
+++ app/src/main/java/com/upsale/upsaleapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.upsale.upsaleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.upsale.upsaleapp.model.Categoria;
 import com.upsale.upsaleapp.ws.WSUpSale;
@@ -15,4 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startFinancas(View view) {
+        Intent ActivityFinancas = new Intent(this, ficancas.class);
+        startActivity(ActivityFinancas);
+    }
+
+    public void startGrafico(View view) {
+        Intent ActivityGrafico = new Intent(this, grafico.class);
+        startActivity(ActivityGrafico);
+    }
+
+    public void startEstoque(View view) {
+        Intent ActivityEstoque = new Intent(this, estoque.class);
+        startActivity(ActivityEstoque);
+    }
+
 }
